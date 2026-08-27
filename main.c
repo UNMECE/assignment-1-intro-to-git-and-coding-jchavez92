fe2b37f2fea7e6e6f1db88cdfaf99d02c563cdb8
John Chavez <jchavez92@dsyslinuxp02.unm.edu>
2026-08-27 14:12:36 -0600

    test commit

---
+++ main.c
@@ -0,0 +1,21 @@
+
+./main
+#include 
+
+int main()
+{	
+	int array1[5];  //create array1
+	int array2[5];  //create array2
+	int array3[5];  //create array3
+	
+
+	for(i=0; i<5; i++)
+	{
+		A1 = avg(array1[i]);	//take average of arra1
+ 
+		A2 = std(array2[i]);
+
+		A3 = sum(array3[i]);
+	{
+}
+
